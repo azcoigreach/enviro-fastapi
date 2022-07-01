8eb66713f792a54fb127a5343ce9fa7dabfbc617
AZcoigreach <azcoigreach@gmail.com>
2022-07-01 10:38:39 -0700

    tmux console

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+REQUIRED_PKG="tmux"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  sudo apt-get --yes install $REQUIRED_PKG
+fi
+
+session="enviro-fastapi"
+tmux has-session -t $session 2>/dev/null
+
+if [ $? != 0 ]; then
+  tmux new-session -d -s $session
+
+  window=0
+  tmux rename-window -t $session:$window 'enviro'
+  tmux send-keys -t $session:$window 'uvicorn app.main:app --host 0.0.0.0 --port 8080 --reload' C-m
+fi
+
+tmux attach-session -t $session
